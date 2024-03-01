02db1f15c08207818bc91aa5d2fd634554babc2d
ticklish-caiman <ticklish.caiman@protonmail.com>
2024-03-01 18:41:35 +0100

    first button

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.write('Hello funga world!')
+
+st.header('First funga button')
+
+if st.button('Answer me!'):
+     st.write('Fun with Genetic Algorithms!')
+else:
+     st.write('What is FunGa?')
