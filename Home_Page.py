84f3c60816b1413ea955e51640e26b9083f92b78
ticklish-caiman <ticklish.caiman@protonmail.com>
2024-03-06 19:06:33 +0100

    login demo update

--- Home_Page.py
+++ Home_Page.py
@@ -43,6 +43,14 @@ def show_sidebar():
             config['preauthorized']
         )
         authenticator.login()
+        if st.session_state["authentication_status"]:
+            authenticator.logout()
+            st.write(f'Welcome *{st.session_state["name"]}*')
+            st.title('Some content')
+        elif st.session_state["authentication_status"] is False:
+            st.error('Username/password is incorrect')
+        elif st.session_state["authentication_status"] is None:
+            st.warning('Please enter your username and password')
 
     elif sidebar == "Button":
         st.header('First funga button')
