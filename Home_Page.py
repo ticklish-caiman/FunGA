9e67e803edced3704a2b1b8facd4dfa6d2998e7e
ticklish-caiman <ticklish.caiman@protonmail.com>
2024-03-07 19:46:54 +0100

    problem spotted

--- Home_Page.py
+++ Home_Page.py
@@ -8,14 +8,7 @@ from utils.custom_css import custom_tabs_css
 
 custom_tabs_css()
 
-if 'kg' not in st.session_state:
-    st.session_state['kg'] = 0.1
-if 'lbs' not in st.session_state:
-    st.session_state['lbs'] = 0.2
 
-# Necessary to prevent streamlit from wiping out session_state when widget gets closed/hidden
-st.session_state.kg = st.session_state.kg
-st.session_state.lbs = st.session_state.lbs
 
 
 show_login_form(gv.authenticator)
@@ -23,3 +16,13 @@ show_register_form(gv.authenticator, gv.config)
 if st.session_state["authentication_status"]:
     show_sidebar()
     show_tabs()
+
+
+# if 'kg' not in st.session_state:
+#     st.session_state['kg'] = 0.1
+# if 'lbs' not in st.session_state:
+#     st.session_state['lbs'] = 0.2
+#
+# # Necessary to prevent streamlit from wiping out session_state when widget gets closed/hidden
+# st.session_state.kg = st.session_state.kg
+# st.session_state.lbs = st.session_state.lbs
--- utils/global_variables.py
+++ utils/global_variables.py
@@ -3,19 +3,20 @@ from yaml.loader import SafeLoader
 import yaml
 from streamlit import set_page_config
 
-# set_page_config has to be here, before every other actions triggerd by other imports
+# set_page_config has to be here, before every other action triggerd by other imports
 set_page_config(page_title="FunGA")
 
 with open('configs/config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
-# For some reason creating authenticator above the functions in auth.py cases it to not initiate properly.
-# Creating it twice for each function raise "multiple widgets with same key" error.
+# For some reason, creating authenticator above the functions in auth.py cases it to not initiate properly.
+# Creating it twice for each function raises "multiple widgets with same key" error.
 # Therefore, to be able to separate login/password_reset/account_creation forms
 # we need to pass authenticator as a separate variable
-# I'm getting very weird behaviour when doing it this way:
+# I'm getting very weird behavior when doing it this way:
 # KeyError: 'st.session_state has no key "authentication_status".
 # Sometimes app throws above error, but after rerun it works -
 # I think closing the browser tab before reset fixes the problem
+# NO IT DOESN'T WORK AFTER REFRESHING THE PAGE!
 
 # Authenticator initialization
 # global authenticator
