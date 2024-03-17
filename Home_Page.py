72f8b2acbcf7c3448562d603b223d83de46c1954
ticklish-caiman <ticklish.caiman@protonmail.com>
2024-03-17 16:55:22 +0100

    add sqlite3

--- Home_Page.py
+++ Home_Page.py
@@ -1,7 +1,10 @@
+import datetime
+
 import streamlit as st
 import streamlit_authenticator as stauth
 from yaml.loader import SafeLoader
 import yaml
+import sqlite3
 
 import gettext
 
@@ -120,8 +123,13 @@ def show_login_form():
 
 show_login_form()
 st.image('img/funGA_logo1.jpg')
-
 if st.session_state["authentication_status"]:
+    # Connect to a database or create it if it doesn't exist
+    con = sqlite3.connect("activity_data.db")
+    # Create cursor required to execute SQL statements
+    cur = con.cursor()
+    cur.execute("CREATE TABLE treevolution(date, seed, result)")
+
     show_sidebar()
     show_tabs()
 
--- pages/1_Fun_and_games.py
+++ pages/1_Fun_and_games.py
@@ -1,4 +1,7 @@
+import sqlite3
+
 import streamlit as st
+import datetime
 from utils.navigation import show_main_menu, get_localizator
 
 _ = get_localizator()
@@ -7,9 +10,22 @@ show_main_menu(_)
 
 tabs_options = ["Treevolution", "Shapevo", "TSP "]
 
+if st.session_state["authentication_status"]:
+    # Connect to a database or create it if it doesn't exist
+    con = sqlite3.connect("activity_data.db")
+    # Create cursor required to execute SQL statements
+    cur = con.cursor()
 tabs = st.tabs(tabs_options)
 with tabs[0]:
     st.header(_('Hello Evolving Trees'))
+    if st.button('Add tree'):
+        cur.execute("""
+            INSERT INTO treevolution VALUES
+                ('2024-03-17:16:33', 42, 112223344)
+        """)
+        con.commit()
+    res = cur.execute("SELECT seed FROM treevolution")
+    st.write("Seeds:", res.fetchall())
 with tabs[1]:
     st.header(_('Hello Evolving Shapes'))
 with tabs[2]:
