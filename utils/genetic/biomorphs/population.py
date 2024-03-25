aeecdeef604a3a0768ca83f664c2330a30489b76
ticklish-caiman <ticklish.caiman@protonmail.com>
2024-03-25 14:15:02 +0100

    simplified biomorph generation
    Our current generator will probably end up as just a subpart of the final generator - maybe a "leg" generator.

--- utils/genetic/biomorphs/population.py
+++ utils/genetic/biomorphs/population.py
@@ -1,3 +1,4 @@
+import math
 import random
 from PIL import Image, ImageDraw
 
@@ -7,26 +8,34 @@ class Biomorph:
         self.parts = []
 
 
-def generate_random_biomorph(start_x=None, start_y=None, shapes_amount=5, img_size=(300, 300), biomorph=None):
-    if biomorph is None:
+def generate_random_biomorph(start_x=None, start_y=None, shapes_amount=5, img_size=(300, 300), new_biomorph=None):
+    if new_biomorph is None:
         new_biomorph = Biomorph()
+        color = random.choice(['red', 'green', 'blue'])
+        width = random.randint(3, 5)
     else:
-        new_biomorph = biomorph
+        color = new_biomorph.parts[0]['color']
+        width = new_biomorph.parts[0]['width']
+
     if start_x is None or start_y is None:
+        # Start at the center if no start coordinates were given
         start_x, start_y = img_size[0] / 2, img_size[1] / 2
 
     # Create the initial line
-    end_x = start_x + random.randint(-50, 50)
-    end_y = start_y + random.randint(-50, 50)
+    length = 50  # Calculate the desired length
+    angle = random.random() * 1 * math.pi  # Random angle (0 to 180 degrees)
+    end_x = int(start_x + length * math.cos(angle))
+    end_y = int(start_y + length * math.sin(angle))
     new_biomorph.parts.append({'type': 'line',
                                'x1': start_x, 'y1': start_y,
                                'x2': end_x, 'y2': end_y,
-                               'width': random.randint(3, 5),  # Line width
-                               'color': random.choice(['red', 'green', 'blue'])})
+                               'width': width,
+                               'color': color})
     print('after append:', new_biomorph.parts)
     print('start_x:', start_x, 'start_y:', start_y, 'end_x:', end_x, 'end_y:', end_y)
     if shapes_amount > 0:
-        last_part = new_biomorph.parts[-1]
+        last_part = new_biomorph.parts[random.randint(-len(new_biomorph.parts), -1)]
+        # last_part = new_biomorph.parts[-1]
         new_start_x = last_part['x2']
         new_start_y = last_part['y2']
         print('new_start_x:', new_start_x, 'new_start_y:', new_start_y)
@@ -56,5 +65,5 @@ def draw_biomorph(biomorph, img_size=(300, 300)):
 
 
 # Example usage (For now, let's print the Biomorph structure)
-my_biomorph = generate_random_biomorph()
+my_biomorph = generate_random_biomorph(shapes_amount=10)
 draw_biomorph(my_biomorph).show()
