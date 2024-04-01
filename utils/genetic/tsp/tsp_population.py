a50d3de13b5015ce29f6fa2b48523ed476a6eb10
ticklish-caiman <ticklish.caiman@protonmail.com>
2024-04-01 03:06:00 +0200

    improve conditional plotting

--- utils/genetic/tsp/tsp_population.py
+++ utils/genetic/tsp/tsp_population.py
@@ -13,13 +13,14 @@ def create_population(pop_size, city_count):
 
 
 # Main GA loop
-population_size = 50
-generations = 10000
+population_size = 10
+generations = 100000
 tournament_size = 3
 mutation_rate = 0.1
 
 population = create_population(population_size, len(cities))
 
+best_distance = float('inf')
 for i in range(generations):
     new_population = []
     for _ in range(int(population_size / 2)):  # Create offspring
@@ -31,10 +32,21 @@ for i in range(generations):
         new_population.extend([child1, child2])
 
     population = new_population
-    # Plotting
-    if i % 300 == 0:  # Plot every 300 iterations
+    best_route = min(population, key=route_distance)
+    current_distance = route_distance(best_route)
+    # Plot at the end
+    if i == generations - 1:
         best_route = min(population, key=route_distance)
         plot_route(best_route, i, cities)
+        print("Plotting progress at iteration:", i, " (", round(i / generations * 100, 1),
+              "% )")  # Show progress percentage
+
+    # Plot and update if there's an improvement, but not more often than 5% of total progress
+    if (i % (generations // 20) == 0) and (current_distance < best_distance):
+        best_distance = current_distance
+        plot_route(best_route, i, cities)
+        print("Plotting progress at iteration:", i, " (", round(i / generations * 100, 1),
+              "% )")  # Show progress percentage
 
 best_route = min(population, key=route_distance)
 print(f"Best route: {best_route}")
