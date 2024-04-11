f6abb2a78e7e215d4c260de2f62766d6208aeb64
ticklish-caiman <ticklish.caiman@protonmail.com>
2024-04-11 21:26:12 +0200

    refactor, custom cities count test

--- pages/3_TSP.py
+++ pages/3_TSP.py
@@ -23,6 +23,10 @@ db_helper = DatabaseHelper('database/data/funga_data.db')
 if 'generations_choice' not in st.session_state:
     st.session_state['generations_choice'] = 500
 
+if 'cities_count' not in st.session_state:
+    st.session_state['cities_count'] = 50
+
+
 if "disabled" not in st.session_state:
     st.session_state["disabled"] = False
 
@@ -35,81 +39,18 @@ def enable():
     st.session_state["disabled"] = False
 
 
-tabs_options = ['Genetic algorithm', 'Create you own route']
-tabs = st.tabs(tabs_options)
+def custom_city_generator():
+    def update_cities(ccc):
+        st.session_state['cities'] = generate_cities(ccc, random_cities)
 
-# TODO: human vs machine, human and machine cooperation
-#       1. give use the ability to find a better solution than GA (vs mode)
-#       2. Give the the ability to help the algorithm (coop mode) - user will be able to add initial solutions
+    custom_cities_count = st.number_input("How many cities:", min_value=5,
+                                          value=51,
+                                          max_value=300,
+                                          key='custom_cities_c')
+    update_cities(custom_cities_count)
 
-with tabs[0]:
-    st.header(_('Traveling Salesman Problem'))
 
-    with st.sidebar.expander("⚙️ TSP OPTIONS️ ⚙️", expanded=True):
-        generations = st.number_input("Generations", min_value=50, value=st.session_state['generations_choice'],
-                                      disabled=st.session_state.disabled)
-
-        with st.popover("🔧 Advanced options", disabled=st.session_state.disabled):
-            col1, col2 = st.columns(2)
-            with col1:
-                pop_size = st.number_input("Population Size", min_value=10, value=50)
-                cities_count = st.number_input("How many cities:", min_value=5, value=50, max_value=300)
-                random_cities = st.checkbox('⬅️ Random cities')
-            with col2:
-                tournament_size = st.number_input("Tournament size:", min_value=2, value=int(pop_size / 10),
-                                                  max_value=pop_size)
-                mutation_rate = st.number_input("Mutation rate:", min_value=0.01, value=0.5, step=0.01)
-
-    with st.expander("What is TSP? (click to expand)", expanded=False):
-        custom_write_style()
-        st.write(
-            """The Traveling Salesman Problem is like finding the shortest way to visit a bunch of cities, going to each 
-            city only once, and ending back where you started.""")
-        st.write('Want to know why it is so hard?')
-        st.page_link('pages/4_Theory.py', label="Click here to see detailed explanation")
-
-    st.write("⤸ Adjust parameters from ⚙️TSP OPTIONS️⚙️ in the sidebar.")
-
-    if st.session_state["disabled"]:
-        st.button("Start again", on_click=enable)
-        st.sidebar.button("Start again", on_click=enable, key='sidebar_reset')
-
-    if st.button("⏱Start Evolution⏱", on_click=disable, disabled=st.session_state.disabled):
-        complete_message = st.empty()
-        with st.spinner("Running Genetic Algorithm..."):
-            cities = generate_cities(cities_count, random_cities)
-            population = create_population(pop_size, len(cities))
-            progress_plot_img = []
-            generator = evolve(population, cities, generations, tournament_size,
-                               mutation_rate)  # Get generator object
-            image_placeholder = st.empty()  # Create a placeholder
-
-            for i, (population, last_image) in enumerate(generator):
-                image_placeholder.image(last_image)  # Display only the new images
-                progress_plot_img.append(last_image)
-
-        if st.session_state["authentication_status"]:
-            print('Saving logged user results...')
-        else:
-            print('Saving guest result...')
-            activity = Activity('test_login', 'TSP', 'TSP_result', user_id=1)
-            db_helper.add_activity(activity)
-
-        complete_message.text("Evolution complete! ✅")
-
-        with st.popover("Show Evolution Process"):
-            for plot_img in progress_plot_img:
-                st.image(plot_img)
-
-    if 'cities' not in st.session_state:
-        st.session_state['cities'] = generate_cities()
-
-    if 'road_clicks' not in st.session_state:
-        st.session_state['road_clicks'] = []
-    if 'user_roads' not in st.session_state:
-        st.session_state['user_roads'] = []
-
-with tabs[1]:
+def custom_city_creator():
     if 'auto_connect_roads' not in st.session_state:
         st.session_state['auto_connect_roads'] = False
 
@@ -129,8 +70,6 @@ with tabs[1]:
     if st.session_state["last_instruction"]:
         st.info(st.session_state["last_instruction"])
 
-
-    @st.cache_data
     def init_user_input_plot():
         # Generate city data and create the Plotly figure (outside draw_map)
         df = pd.DataFrame(st.session_state['cities'], columns=['x', 'y'])
@@ -156,11 +95,8 @@ with tabs[1]:
 
         return fig
 
-
     fig = init_user_input_plot()
 
-
-    @st.experimental_fragment
     def draw_map():
         # Draw all existing roads
         for start, end in st.session_state['user_roads']:
@@ -168,7 +104,6 @@ with tabs[1]:
                 go.Scatter(x=[start[0], end[0]], y=[start[1], end[1]], mode='lines', line=dict(color='red'))
             )
         return fig
-
 
     button_col1, button_col2 = st.columns(2)
 
@@ -183,6 +118,7 @@ with tabs[1]:
     with button_col2:
 
         if st.button("Clear roads (start again)"):
+            # TODO: fix bug
             if len(st.session_state.user_roads) > 0:
                 st.session_state['user_roads'] = []
                 st.session_state['road_clicks'] = []
@@ -240,7 +176,6 @@ with tabs[1]:
                             st.session_state['cities'])
                     else:
 
-
                         st.session_state["last_error"] = "Changes would create sub-routes!"
 
                     # prevent creating separate routes
@@ -271,3 +206,84 @@ with tabs[1]:
 
     if rerun:
         st.rerun()
+
+
+tabs_options = ['Genetic algorithm', 'Create you own route']
+tabs = st.tabs(tabs_options)
+
+# TODO: human vs machine, human and machine cooperation
+#       1. give use the ability to find a better solution than GA (vs mode)
+#       2. Give the the ability to help the algorithm (coop mode) - user will be able to add initial solutions
+
+with tabs[0]:
+    st.header(_('Traveling Salesman Problem'))
+
+    with st.sidebar.expander("⚙️ TSP OPTIONS️ ⚙️", expanded=True):
+        generations = st.number_input("Generations", min_value=50, value=st.session_state['generations_choice'],
+                                      disabled=st.session_state.disabled)
+
+        with st.popover("🔧 Advanced options", disabled=st.session_state.disabled):
+            col1, col2 = st.columns(2)
+            with col1:
+                pop_size = st.number_input("Population Size", min_value=10, value=50)
+                st.session_state['cities_count'] = st.number_input("How many cities:", min_value=5,
+                                                                   value=st.session_state['cities_count'],
+                                                                   max_value=300)
+                random_cities = st.checkbox('⬅️ Random cities')
+            with col2:
+                tournament_size = st.number_input("Tournament size:", min_value=2, value=int(pop_size / 10),
+                                                  max_value=pop_size)
+                mutation_rate = st.number_input("Mutation rate:", min_value=0.01, value=0.5, step=0.01)
+
+    with st.expander("What is TSP? (click to expand)", expanded=False):
+        custom_write_style()
+        st.write(
+            """The Traveling Salesman Problem is like finding the shortest way to visit a bunch of cities, going to each 
+            city only once, and ending back where you started.""")
+        st.write('Want to know why it is so hard?')
+        st.page_link('pages/4_Theory.py', label="Click here to see detailed explanation")
+
+    st.write("⤸ Adjust parameters from ⚙️TSP OPTIONS️⚙️ in the sidebar.")
+
+    if st.session_state["disabled"]:
+        st.button("Start again", on_click=enable)
+        st.sidebar.button("Start again", on_click=enable, key='sidebar_reset')
+
+    if st.button("⏱Start Evolution⏱", on_click=disable, disabled=st.session_state.disabled):
+        complete_message = st.empty()
+        with st.spinner("Running Genetic Algorithm..."):
+            cities = generate_cities(st.session_state['cities_count'], random_cities)
+            population = create_population(pop_size, len(cities))
+            progress_plot_img = []
+            generator = evolve(population, cities, generations, tournament_size,
+                               mutation_rate)  # Get generator object
+            image_placeholder = st.empty()  # Create a placeholder
+
+            for i, (population, last_image) in enumerate(generator):
+                image_placeholder.image(last_image)  # Display only the new images
+                progress_plot_img.append(last_image)
+
+        if st.session_state["authentication_status"]:
+            print('Saving logged user results...')
+        else:
+            print('Saving guest result...')
+            activity = Activity('test_login', 'TSP', 'TSP_result', user_id=1)
+            db_helper.add_activity(activity)
+
+        complete_message.text("Evolution complete! ✅")
+
+        with st.popover("Show Evolution Process"):
+            for plot_img in progress_plot_img:
+                st.image(plot_img)
+
+    if 'cities' not in st.session_state:
+        st.session_state['cities'] = generate_cities()
+
+    if 'road_clicks' not in st.session_state:
+        st.session_state['road_clicks'] = []
+    if 'user_roads' not in st.session_state:
+        st.session_state['user_roads'] = []
+
+with tabs[1]:
+    custom_city_generator()
+    custom_city_creator()
